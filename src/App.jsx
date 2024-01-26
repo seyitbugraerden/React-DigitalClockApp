1be268651ed8556e5812ce5344a03814fc89da14
Bugra <seyitbugraerden@gmail.com>
2024-01-26 10:39:34 +0300

    Global Clock API

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
 import { useState } from "react";
 import { Panel } from "primereact/panel";
+import { InputText } from "primereact/inputtext";
 import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [selectedCity, setSelectedCity] = useState("");
+  const [values, setValues] = useState([]);
+
+  const searchTime = () => {
+    axios
+      .get(`https://api.api-ninjas.com/v1/worldtime?city=${selectedCity}`, {
+        headers: {
+          "X-Api-Key": "TyvX7NrvmCbJ9xsiNW6f4A==ioWR7qO7m2TsUYMx",
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        setValues(response.data);
+        console.log(values);
+      })
+      .catch((error) => {
+        console.error("Error fetching time:", error);
+      });
+  };
 
   return (
     <>
       <Panel header="Header">
-        <p className="m-0">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <span className="p-float-label">
+          <InputText
+            id="username"
+            value={selectedCity}
+            onChange={(e) => setSelectedCity(e.target.value)}
+          />
+          <label htmlFor="username">Select City</label>
+        </span>
+        <br />
+        <h3>
+          {values.hour} : {values.minute} : {values.second}
+        </h3>
+        <button onClick={searchTime}>Search</button>
       </Panel>
     </>
   );
